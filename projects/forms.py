f87cc63ab725b806e35d21b6b0dca11a277c85fd
xRagnarekx <agevent@mail.ru>
2026-05-07 19:03:08 +0600

    справил отображение статики в Docker и обновил Dockerfile

--- projects/forms.py
+++ projects/forms.py
@@ -6,3 +6,12 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['name', 'description', 'github_url', 'image']
+
+
+labels = {
+    'name': 'Название',
+    'description': 'Описание',
+    'github_url': 'Ссылка на GitHub',
+    'image': 'Обложка проекта',
+    'status': 'Статус',
+}
